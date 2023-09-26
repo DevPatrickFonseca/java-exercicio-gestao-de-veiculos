f68c5ba0d7b1d7c5771f7b4151d515761c5d7404
PFonsecaFV <pfonsecafv@gmail.com>
2023-09-25 23:13:29 -0300

    ✨ Req 01 - Calcular o consumo de combustível do Carro

--- src/main/java/com/trybe/acc/java/Carro.java
+++ src/main/java/com/trybe/acc/java/Carro.java
@@ -9,5 +9,46 @@ public class Carro extends VeiculoBase {
   /**
    * Atributos e Métodos.
    */
+  private final int quantidadePassageiros;
 
+  /**
+   * Construtor.
+   */
+  public Carro(
+      String marca,
+      String modelo,
+      int ano,
+      String tipoCombustivel,
+      int capacidadeTanque,
+      int quantidadePassageiros
+  ) {
+    super(marca, modelo, ano, tipoCombustivel, capacidadeTanque);
+    this.quantidadePassageiros = quantidadePassageiros;
+  }
+
+  @Override
+  public double calcularConsumoCombustivel(double distancia) {
+    double consumo = 10.0;
+    return distancia / consumo;
+  }
+
+  @Override
+  public void exibirInformacoes() {
+    System.out.println("        Veículo: Carro");
+    System.out.println("██████████████████████████████");
+    System.out.println("Marca: " + getMarca());
+    System.out.println("Modelo: " + getModelo());
+    System.out.println("Ano: " + getAno());
+    System.out.println("Tipo de Combustível: " + getTipoCombustivel());
+    System.out.println("Capacidade do Tanque: " + getCapacidadeTanque());
+    System.out.println("Quantidade de Passageiros: " + getQuantidadePassageiros());
+    System.out.println("██████████████████████████████");
+  }
+
+  /**
+   * Getters.
+   */
+  public int getQuantidadePassageiros() {
+    return quantidadePassageiros;
+  }
 }
--- src/main/java/com/trybe/acc/java/VeiculoBase.java
+++ src/main/java/com/trybe/acc/java/VeiculoBase.java
@@ -6,8 +6,69 @@ package com.trybe.acc.java;
 
 public abstract class VeiculoBase implements Veiculo {
 
+  private String marca;
+  private String modelo;
+  private int ano;
+  private String tipoCombustivel;
+  private int capacidadeTanque;
+
+  /**
+   * Construtor.
+   */
+  public VeiculoBase(
+      String marca,
+      String modelo,
+      int ano,
+      String tipoCombustivel,
+      int capacidadeTanque
+  ) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.ano = ano;
+    this.tipoCombustivel = tipoCombustivel;
+    this.capacidadeTanque = capacidadeTanque;
+  }
+
   /**
    * Atributos e Métodos.
    */
+  public String getMarca() {
+    return marca;
+  }
 
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public String getModelo() {
+    return modelo;
+  }
+
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+
+  public int getAno() {
+    return ano;
+  }
+
+  public void setAno(int ano) {
+    this.ano = ano;
+  }
+
+  public String getTipoCombustivel() {
+    return tipoCombustivel;
+  }
+
+  public void setTipoCombustivel(String tipoCombustivel) {
+    this.tipoCombustivel = tipoCombustivel;
+  }
+
+  public int getCapacidadeTanque() {
+    return capacidadeTanque;
+  }
+
+  public void setCapacidadeTanque(int capacidadeTanque) {
+    this.capacidadeTanque = capacidadeTanque;
+  }
 }
