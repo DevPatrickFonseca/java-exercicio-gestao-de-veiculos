2608f1e0bd8da3aceee853130642880a92fd684b
PFonsecaFV <pfonsecafv@gmail.com>
2023-09-25 23:13:44 -0300

    ✨ Req 02 - Calcular o consumo de combustível do Caminhão

--- src/main/java/com/trybe/acc/java/Caminhao.java
+++ src/main/java/com/trybe/acc/java/Caminhao.java
@@ -9,5 +9,47 @@ public class Caminhao extends VeiculoBase {
   /**
    * Atributos e Métodos.
    */
+  private final double capacidadeDeCarga;
 
+  /**
+   * Construtor.
+   */
+  public Caminhao(
+      String marca,
+      String modelo,
+      int ano,
+      String tipoCombustivel,
+      int capacidadeTanque,
+      double capacidadeDeCarga
+  ) {
+    super(marca, modelo, ano, tipoCombustivel, capacidadeTanque);
+    this.capacidadeDeCarga = capacidadeDeCarga;
+  }
+
+
+  @Override
+  public double calcularConsumoCombustivel(double distancia) {
+    double consumo = 6.0;
+    return distancia / consumo;
+  }
+
+  @Override
+  public void exibirInformacoes() {
+    System.out.println("      Veículo: Caminhão");
+    System.out.println("██████████████████████████████");
+    System.out.println("Marca: " + getMarca());
+    System.out.println("Modelo: " + getModelo());
+    System.out.println("Ano: " + getAno());
+    System.out.println("Tipo de Combustível: " + getTipoCombustivel());
+    System.out.println("Capacidade do Tanque: " + getCapacidadeTanque());
+    System.out.println("Capacidade de Carga: " + getCapacidadeDeCarga());
+    System.out.println("██████████████████████████████");
+  }
+
+  /**
+   * Getters.
+   */
+  public double getCapacidadeDeCarga() {
+    return capacidadeDeCarga;
+  }
 }
