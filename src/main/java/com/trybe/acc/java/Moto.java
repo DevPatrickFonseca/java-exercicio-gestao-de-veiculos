27446c463adb6e0176a7ca9fd19017a7ab9c5316
PFonsecaFV <pfonsecafv@gmail.com>
2023-09-25 23:14:12 -0300

    ✨ Req 03 - Calcular o consumo de combustível da Moto

--- src/main/java/com/trybe/acc/java/Moto.java
+++ src/main/java/com/trybe/acc/java/Moto.java
@@ -10,4 +10,46 @@ public class Moto extends VeiculoBase {
    * Atributos e Métodos.
    */
 
+  private final String tipoDeMoto;
+
+  /**
+   * Construtor.
+   */
+  public Moto(
+      String marca,
+      String modelo,
+      int ano,
+      String tipoCombustivel,
+      int capacidadeTanque,
+      String tipoDeMoto
+  ) {
+    super(marca, modelo, ano, tipoCombustivel, capacidadeTanque);
+    this.tipoDeMoto = tipoDeMoto;
+  }
+
+  @Override
+  public double calcularConsumoCombustivel(double distancia) {
+    double consumo = 18.0;
+    return distancia / consumo;
+  }
+
+  @Override
+  public void exibirInformacoes() {
+    System.out.println("        Veículo: Moto");
+    System.out.println("██████████████████████████████");
+    System.out.println("Marca: " + getMarca());
+    System.out.println("Modelo: " + getModelo());
+    System.out.println("Ano: " + getAno());
+    System.out.println("Tipo de Combustível: " + getTipoCombustivel());
+    System.out.println("Capacidade do Tanque: " + getCapacidadeTanque());
+    System.out.println("Tipo de moto: " + getTipoDeMoto());
+    System.out.println("██████████████████████████████");
+  }
+
+  /**
+   * Getters.
+   */
+  public String getTipoDeMoto() {
+    return tipoDeMoto;
+  }
 }
